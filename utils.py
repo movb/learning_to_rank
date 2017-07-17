2d0b81d0c680f918de70865a2719a1a35ee425b0
Mike Plekhanov <mike.plekhanov@gmail.com>
2017-07-17 02:54:34 +0000

    Utils

---
+++ utils.py
@@ -0,0 +1,58 @@
+import pickle
+
+def get_document(fd, index, doc_id):
+    pos = index[doc_id]
+    fd.seek(pos)
+    line = fd.readline()
+    parts = line.strip().split('\t')
+    
+    return parts[1], parts[2]
+
+
+def save_to_file(obj, file_name):
+    with open(file_name, "wb") as fd:
+        pickle.dump(obj, fd)
+        
+
+def load_from_file(file_name):
+    with open(file_name, "rb") as fd:
+        return pickle.load(fd)
+
+
+def load_queries(file_name):
+    queries = {}
+    with open(file_name, 'rt') as fd:
+        for line in fd:
+            parts = line.strip().split('\t')
+            queries[int(parts[0])] = parts[1]
+    
+    return queries
+
+
+def load_test(file_name):
+    queries = []
+    with open(file_name, 'rt') as fd:
+        for line in fd:
+            parts = line.strip().split('\t')
+            q_id = int(parts[0])
+            docs = eval(parts[1])
+            
+            queries.append((q_id, docs))
+    return queries
+
+def copy_model(old, new):
+    new.file_name = old.file_name
+    new.norm_method = old.norm_method
+    new.debug = old.debug
+    new.save_normalized = old.save_normalized
+    new.title_terms = old.title_terms
+    new.body_terms = old.body_terms
+    new.term_to_title = old.term_to_title
+    new.term_to_body = old.term_to_body
+    new.morph = old.morph
+
+    new.N = old.N
+    new.title_lengths = old.title_lengths
+    new.body_lengths = old.body_lengths
+    new.avg_title_length = old.avg_title_length
+    new.avg_body_length = old.avg_body_length
